50401257ffba77750f4dd558dcaa6f767cb4b0d8
Manish Kumar <manishkumarg1609@gmail.com>
2026-09-19 12:38:52 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let player=document.querySelector(".player");
+let enemy=document.querySelector(".enemy");
 let moveX=100;
 var moveY=0;
 document.addEventListener("keydown",function(move){
@@ -42,18 +43,53 @@ document.addEventListener("keydown",function(attack){
 
         setTimeout(function(){
             playerImg.src="assets/Player/Attack/Attack_06.png";
+            checkHit();
         },500);
         setTimeout(function(){
             playerImg.src="assets/Player_Idle.svg";
         },600);
         
+        function checkHit(){
+            let playerPos=player.offsetLeft;
+            let enemyPos=enemy.offsetLeft;
+            let distance=Math.abs(playerPos-enemyPos);
+            if(distance<150){
+                let enemyImg=document.querySelector(".enemy img");
+
+                enemyImg.src="assets/Enemy/Hit/Hit_01.png";
+        setTimeout(function(){
+            enemyImg.src="assets/Enemy/Hit/Hit_02.png";
+        },100);
+
+        setTimeout(function(){
+            enemyImg.src="assets/Enemy/Hit/Hit_03.png";
+        },200);
+
+        setTimeout(function(){
+            enemyImg.src="assets/Enemy/Hit/Hit_04.png";
+        },300);
+
+        setTimeout(function(){
+            enemyImg.src="assets/Enemy/Hit/Hit_05.png";
+        },400);
+
+        setTimeout(function(){
+            enemyImg.src="assets/Enemy/Hit/Hit_06.png";
+        },500);
+        setTimeout(function(){
+            enemyImg.src="assets/Enemy_Idle (1).svg";
+        },600);
+
+            }
+        }
+
     }
 });
 
-var playerImg=document.querySelector(".player img");
-document.addEventListener("keydown",function(attack){
-    if(attack.key==="d" || attack.key==="ArrowRight"){
-        playerImg.src="assets/Player/_01.png";
+
+document.addEventListener("keydown",function(jump){
+    if(jump.key==="w" || jump.key==="ArrowUp"){
+        playerImg.src="assets/Player/Jump/Jump_01.png";
         setTimeout(function(){
             playerImg.src="assets/Player/Jump/Jump_02.png";
         },100);
@@ -69,9 +105,34 @@ document.addEventListener("keydown",function(attack){
         setTimeout(function(){
             playerImg.src="assets/Player/Jump/Jump_05.png";
         },400);
+        setTimeout(function(){
+            playerImg.src="assets/Player_Idle.svg";
+        },500);
+        
+    }
+});
+
+document.addEventListener("keydown",function(run){
+    if(run.key==="s" || run.key==="Shift"){
+        playerImg.src="assets/Player/Run/Run_01.png";
+        setTimeout(function(){
+            playerImg.src="assets/Player/Run/Run_02.png";
+        },100);
 
         setTimeout(function(){
-            playerImg.src="assets/Player/Jump/Jump_06.png";
+            playerImg.src="assets/Player/Run/Run_03.png";
+        },200);
+
+        setTimeout(function(){
+            playerImg.src="assets/Player/Run/Run_04.png";
+        },300);
+
+        setTimeout(function(){
+            playerImg.src="assets/Player/Run/Run_05.png";
+        },400);
+
+        setTimeout(function(){
+            playerImg.src="assets/Player/Run/Run_06.png";
         },500);
         setTimeout(function(){
             playerImg.src="assets/Player_Idle.svg";
@@ -79,3 +140,36 @@ document.addEventListener("keydown",function(attack){
         
     }
 });
+
+document.addEventListener("keydown",function(walk){
+    if(walk.key==="d" || walk.key==="ArrowRight"){
+        playerImg.src="assets/Player/Walk/Walk_01.png";
+        setTimeout(function(){
+            playerImg.src="assets/Player/Walk/Walk_02.png";
+        },100);
+
+        setTimeout(function(){
+            playerImg.src="assets/Player/Walk/Walk_03.png";
+        },200);
+
+        setTimeout(function(){
+            playerImg.src="assets/Player/Walk/Walk_04.png";
+        },300);
+
+        setTimeout(function(){
+            playerImg.src="assets/Player/Walk/Walk_05.png";
+        },400);
+
+        setTimeout(function(){
+            playerImg.src="assets/Player/Walk/Walk_06.png";
+        },500);
+        setTimeout(function(){
+            playerImg.src="assets/Player/Walk/Walk_07.png";
+        },600);
+        setTimeout(function(){
+            playerImg.src="assets/Player_Idle.svg";
+        },700);
+        
+    }
+});
+
